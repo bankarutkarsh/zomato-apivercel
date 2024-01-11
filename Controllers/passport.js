1c9e08a15ecde17ffd7b3101b53232cca22af658
Utkarsh <utkarshbankar07@gmail.com>
2024-01-11 19:53:40 +0530

    dotenv

--- Controllers/passport.js
+++ Controllers/passport.js
@@ -1,11 +1,14 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
+const dotenv = require('dotenv')
 const passport = require('passport');
 
- 
+dotenv.config();
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
     callbackURL: "/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
